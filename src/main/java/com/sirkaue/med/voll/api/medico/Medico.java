bbfd89bffea217230e84b7fab13c1fe0514e9d77
sirkaue <kauekdslopes@gmail.com>
2024-07-06 14:47:40 -0300

    mapeando entidades

---
+++ src/main/java/com/sirkaue/med/voll/api/medico/Medico.java
@@ -0,0 +1,30 @@
+package com.sirkaue.med.voll.api.medico;
+
+import com.sirkaue.med.voll.api.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
