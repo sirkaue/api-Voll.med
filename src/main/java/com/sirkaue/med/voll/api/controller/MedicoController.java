701429fc0af6770ad854a221a5a770efdc170913
sirkaue <kauekdslopes@gmail.com>
2024-07-06 17:22:39 -0300

    Migrations com Flyway

--- src/main/java/com/sirkaue/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/sirkaue/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import com.sirkaue.med.voll.api.medico.DadosCadastroMedico;
 import com.sirkaue.med.voll.api.medico.Medico;
 import com.sirkaue.med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
