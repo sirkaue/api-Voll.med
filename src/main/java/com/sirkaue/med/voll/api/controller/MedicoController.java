bbdc3b3da982a1dccc062daaf0f2c68131b632c0
sirkaue <kauekdslopes@gmail.com>
2024-07-07 13:51:45 -0300

    paginação

--- src/main/java/com/sirkaue/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/sirkaue/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,10 @@ import com.sirkaue.med.voll.api.medico.Medico;
 import com.sirkaue.med.voll.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable pageable) {
+        return repository.findAll(pageable).map(DadosListagemMedico::new);
     }
 }
