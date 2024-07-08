f9642c8f7522615e6c52a4f73a6d4a067292fae7
sirkaue <kauekdslopes@gmail.com>
2024-07-08 15:31:20 -0300

    reponse pacientes

---
+++ src/main/java/com/sirkaue/med/voll/api/pacientes/DadosDetalhamentoPaciente.java
@@ -0,0 +1,21 @@
+package com.sirkaue.med.voll.api.pacientes;
+
+import com.sirkaue.med.voll.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        String telefone,
+        Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(),
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getCpf(),
+                paciente.getTelefone(),
+                paciente.getEndereco());
+    }
+}
