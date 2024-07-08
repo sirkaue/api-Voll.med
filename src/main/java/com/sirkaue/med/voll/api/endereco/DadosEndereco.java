01d31ddb83e57bc203791b02c0d4f8116529cbf2
sirkaue <kauekdslopes@gmail.com>
2024-07-08 13:56:44 -0300

    corrigindo erro

--- src/main/java/com/sirkaue/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/com/sirkaue/med/voll/api/endereco/DadosEndereco.java
@@ -12,7 +12,7 @@ public record DadosEndereco(
         String bairro,
 
         @NotBlank
-        @Pattern(regexp = "\\d {8}")
+        @Pattern(regexp = "\\d{8}")
         String cep,
 
         @NotBlank
