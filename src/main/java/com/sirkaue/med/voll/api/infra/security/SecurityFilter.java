78e9a1e32e0bef8c069cb58fd3c35cf25fb0a2b1
sirkaue <kauekdslopes@gmail.com>
2024-07-09 17:30:39 -0300

    recuperando token

--- src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
@@ -13,7 +13,18 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
 
+        System.out.println(tokenJwt);
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
