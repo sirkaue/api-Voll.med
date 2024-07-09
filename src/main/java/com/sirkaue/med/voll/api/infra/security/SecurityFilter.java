dc5476de0e7a539b20ec089d6a62c67aadbec494
sirkaue <kauekdslopes@gmail.com>
2024-07-09 17:41:04 -0300

    validando token

--- src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,11 +12,15 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJwt = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJwt);
 
-        System.out.println(tokenJwt);
         filterChain.doFilter(request, response);
     }
 
