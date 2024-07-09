90f99f1d6f8c6d5112d4efd822c034ff877816a8
sirkaue <kauekdslopes@gmail.com>
2024-07-09 18:06:11 -0300

    autenticando usuario

--- src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityConfigurations.java
@@ -19,6 +19,10 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/login").permitAll();
+                    req.anyRequest().authenticated();
+                })
                 .build();
     }
 
--- src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/com/sirkaue/med/voll/api/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package com.sirkaue.med.voll.api.infra.security;
 
+import com.sirkaue.med.voll.api.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,20 +19,29 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJwt = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJwt);
 
+        if (tokenJwt != null) {
+            var subject = tokenService.getSubject(tokenJwt);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
