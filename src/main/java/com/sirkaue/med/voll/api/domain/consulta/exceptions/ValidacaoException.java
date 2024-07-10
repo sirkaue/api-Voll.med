d42120e9199978315afc8e721a591ac7f25f6a3e
sirkaue <kauekdslopes@gmail.com>
2024-07-10 13:30:05 -0300

    exception

---
+++ src/main/java/com/sirkaue/med/voll/api/domain/consulta/exceptions/ValidacaoException.java
@@ -0,0 +1,7 @@
+package com.sirkaue.med.voll.api.domain.consulta.exceptions;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String s) {
+        super(s);
+    }
+}
